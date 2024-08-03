d84fa40b676c39191336231d8e5c74404b4f8f9f
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-08-03 23:52:28 +0300

    Support the toast function

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,14 @@
+import { Toaster } from "@/components/ui/toaster";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex-center bg-primary-50 bg-dotted-pattern absolute left-0 top-0 z-[-1] min-h-screen w-full bg-cover bg-fixed bg-center">
+    <div className="flex-center bg-primary-50 absolute left-0 top-0 z-[-1] min-h-screen w-full bg-dotted-pattern bg-cover bg-fixed bg-center">
       {children}
+      <Toaster />
     </div>
   );
 }
