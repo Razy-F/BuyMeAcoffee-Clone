eedbafa5833fc4d9131243ef71af0ce33ac43472
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-08-05 23:55:40 +0300

    The file auth.config.ts was added.

---
+++ src/auth.config.ts
@@ -0,0 +1,21 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isProtectedPath = nextUrl.pathname.startsWith("/protected");
+      if (isProtectedPath) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
