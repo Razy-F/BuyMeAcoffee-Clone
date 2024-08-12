0a646d1fff36313f6731ad5742e428f9820c21fd
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-08-13 00:31:53 +0300

    Header section display in guest/auth mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,20 @@ import Link from "next/link";
 import { LiaMugHotSolid } from "react-icons/lia";
 
 import { Button } from "./ui/button";
+import { auth, signOut } from "@/auth";
+import { FaUser } from "react-icons/fa";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-const Header = () => {
+const Header = async () => {
+  const session = await auth();
+  const firstName = session?.user?.name?.split(" ")[0];
   return (
     <header className="mb-16">
       <div className="mx-auto flex max-w-2xl justify-between px-4 py-4">
@@ -17,15 +29,47 @@ const Header = () => {
           <Link href={"/about"}>FAQ</Link>
           <Link href={"/about"}>Contact</Link>
           <div className="flex gap-4">
-            <Button
-              className="ml-4 rounded-full border-2 bg-transparent text-foreground"
-              asChild
-            >
-              <Link href={"/log-in"}>Log in</Link>
-            </Button>
-            <Button className="rounded-full" asChild>
-              <Link href={"/sign-in"}>Sign up</Link>
-            </Button>
+            {session && (
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button variant="outline" className="gap-2">
+                    <FaUser />
+                    My Account
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel className="flex gap-2">
+                    {firstName} ☕
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <form
+                      action={async () => {
+                        "use server";
+                        await signOut();
+                      }}
+                      className="w-full"
+                    >
+                      <Button className="w-full">Log out</Button>
+                    </form>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+            {!session && (
+              <>
+                <Button
+                  className="ml-4 rounded-full border-2 bg-transparent text-foreground"
+                  asChild
+                >
+                  <Link href={"/log-in"}>Log in</Link>
+                </Button>
+                <Button className="rounded-full" asChild>
+                  <Link href={"/sign-in"}>Sign up</Link>
+                </Button>
+              </>
+            )}
           </div>
         </nav>
       </div>
